99fec4db66dab52f9687f4c641b4ca187a46c618
Abhishek kurmatkar <97021984+Abhi84481@users.noreply.github.com>
2024-10-24 20:56:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 resource "google_storage_bucket" "test_bucket" {
   name     = "tfe-test-bucket-${random_id.bucket_id.hex}"
-  location = "ME"
+  location = "ME-CENTRAL2"
 }
 
 resource "random_id" "bucket_id" {
